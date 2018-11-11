0428abd5e702c4e1b49dd3f04614a24f8005e80d
Jason Brown <jrbrowntc@gmail.com>
2018-11-11 14:54:27 -0500

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Jason Brown on 11/11/18.
+//  Copyright © 2018 Jason Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
